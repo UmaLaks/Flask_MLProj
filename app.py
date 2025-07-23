7d66b2356550dc9342151b8c9e9737ba7989e339
DEVU UMA LAKSHMI <devuumalakshmi@gmail.com>
2025-07-24 00:11:23 +0530

    Adding logss

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-
+import logging
 from flask import Flask,request,jsonify
 import pickle
 import numpy as np
 app=Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s [%(levelname)s] %(message)s')
+
 @app.route('/predict',methods=["post"])
 def predict():
+    logging.info("Flask app is running")
     data=request.get_json()
+    logging.info("Getting Data from the websever")
     data_list=data['features']
     data_np=np.array(data_list).reshape(1,-1)
+    logging.info("fetching the data to train the model")
     model=pickle.load(open("grid.pkl", "rb"))
+    logging.info("Loading the model")
     y_pred=model.predict(data_np)
+    print(y_pred.tolist())
+    logging.info("prediction output")
     return jsonify({"p":y_pred.tolist()})
 
 
